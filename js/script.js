2e6fadd39f37146d802441988785a48300eaa941
RicTieri <tieri.riccardo@gmail.com>
2023-11-14 14:36:28 +0100

    added style and fizzbuzz

--- js/script.js
+++ js/script.js
@@ -10,14 +10,17 @@ for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
     headingCardElement.classList.add("green")
     headingCardElement.innerHTML = 'fizz';
-  } else  if (i % 5 === 0) {
+  } 
+  if (i % 5 === 0) {
     headingCardElement.classList.add("yellow")
     headingCardElement.innerHTML = 'buzz';
+  } 
+  if (i % 3 === 0 && i % 5 === 0){
+    headingCardElement.classList.add("orange")
+    headingCardElement.innerHTML = 'fizzbuzz';
   }
 
   cardElement.appendChild(headingCardElement);
   wrapperElement.appendChild(cardElement);
-
-  console.log(i)
 }
 
