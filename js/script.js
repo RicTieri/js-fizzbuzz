143f42959e141810af9f52d64ac8e867d8099951
RicTieri <tieri.riccardo@gmail.com>
2023-11-14 14:28:52 +0100

    added function for fizz and buzz

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 const wrapperElement = document.querySelector('div.wrapper');
 
-for ( let i = 0; i <=20 ; i = i + 2){
+for (let i = 1; i <= 100; i++) {
   const cardElement = document.createElement('div');
   cardElement.className = 'card';
 
-  const headingCardElement = document.createElement('h2');
+  const headingCardElement = document.createElement('h2')
   headingCardElement.innerHTML = i;
+
+  if (i % 3 === 0) {
+    headingCardElement.classList.add("green")
+    headingCardElement.innerHTML = 'fizz';
+  } else  if (i % 5 === 0) {
+    headingCardElement.classList.add("yellow")
+    headingCardElement.innerHTML = 'buzz';
+  }
 
   cardElement.appendChild(headingCardElement);
   wrapperElement.appendChild(cardElement);
